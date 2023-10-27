7a1101b7eaf3a3de95dcffd4fc51a5035db4f728
Figleo <kanchis@bk.ru>
2023-10-27 21:29:22 +0300

    Accent comments

--- Content.Server/_WL/Speech/EntitySystems/KidanAccentSystem.cs
+++ Content.Server/_WL/Speech/EntitySystems/KidanAccentSystem.cs
@@ -19,37 +19,36 @@ public sealed class KidanAccentSystem : EntitySystem
     {
         var message = args.Message;
 
-        // c => ссс
         message = Regex.Replace(
             message,
             "с+",
             _random.Pick(new List<string>() { "з", "зз" })
         );
-        // С => CCC
+
         message = Regex.Replace(
             message,
             "С+",
             _random.Pick(new List<string>() { "З", "ЗЗ" })
         );
-        // з => ссс
+
         message = Regex.Replace(
             message,
             "з+",
             _random.Pick(new List<string>() { "зз", "ззз" })
         );
-        // З => CCC
+
         message = Regex.Replace(
             message,
             "З+",
             _random.Pick(new List<string>() { "ЗЗ", "ЗЗЗ" })
         );
-        // ш => шшш
+
         message = Regex.Replace(
             message,
             "ж+",
             _random.Pick(new List<string>() { "жж", "жжж" })
         );
-        // Ш => ШШШ
+
         message = Regex.Replace(
             message,
             "Ж+",
