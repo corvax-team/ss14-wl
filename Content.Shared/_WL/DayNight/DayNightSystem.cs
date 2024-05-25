5dd03e383a2a1b720131ca76574ed7fe69263be4
Pupchansky <puppochta@gmail.com>
2024-05-25 14:55:44 +0500

    Added mechanics of day and night shift

---
+++ Content.Shared/_WL/DayNight/DayNightSystem.cs
@@ -0,0 +1,96 @@
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+using System.Linq;
+using System.Numerics;
+
+namespace Content.Shared._WL.DayNight
+{
+    public sealed partial class DayNightSystem : EntitySystem
+    {
+        [Dependency] private readonly IGameTiming _gameTime = default!;
+        [Dependency] private readonly IPrototypeManager _protoMan = default!;
+        [Dependency] private readonly SharedMapSystem _mapSys = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<DayNightComponent, MapInitEvent>(OnMapInit, after: [typeof(SharedMapSystem)]);
+        }
+
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            var query = EntityQueryEnumerator<MapComponent>();
+            while (query.MoveNext(out var map, out var mapComponent))
+            {
+                if (!TryComp<MapLightComponent>(map, out var mapLightComp))
+                    return;
+
+                if (!TryComp<DayNightComponent>(map, out var dayNightComp))
+                    return;
+
+                if (!dayNightComp.WasInit || mapComponent.MapPaused)
+                    continue;
+
+                if (_gameTime.CurTime >= dayNightComp.NextCycle)
+                    dayNightComp.NextCycle += dayNightComp.FullCycle;
+
+                var color = CalculateColor(
+                    _gameTime.CurTime,
+                    dayNightComp.FullCycle,
+                    dayNightComp.NextCycle,
+                    Color.FromHex(dayNightComp.DayHex),
+                    Color.FromHex(dayNightComp.NightHex),
+                    dayNightComp.DayNightRatio);
+
+                if (color == mapLightComp.AmbientLightColor) //Оптимизация для случаев, если цикл дня и ночи огромен.
+                    continue;
+
+                _mapSys.SetAmbientLight(mapComponent.MapId, color);
+            }
+        }
+
+        private void OnMapInit(EntityUid station, DayNightComponent comp, MapInitEvent args)
+        {
+            if (!TryComp<MapComponent>(station, out var mapComponent))
+                return;
+
+            _mapSys.SetAmbientLight(mapComponent.MapId, Color.FromHex(comp.DayHex));
+            comp.NextCycle = _gameTime.CurTime + comp.FullCycle;
+            comp.WasInit = true;
+        }
+
+        public static Color CalculateColor(TimeSpan currentTime, TimeSpan fullCycle, TimeSpan nextCycle, Color dayColor, Color nightColor, Vector2 dayNightRatio)
+        {
+            currentTime = currentTime - (nextCycle - fullCycle);
+
+            var pair = dayNightRatio.X + dayNightRatio.Y;
+
+            var dayTime = fullCycle.TotalMinutes / pair * dayNightRatio.X;
+            var nightTime = fullCycle.TotalMinutes / pair * dayNightRatio.Y;
+
+            var isDay = currentTime.TotalMinutes <= dayTime;
+
+            var filledPercentage = isDay
+                ? currentTime.TotalMinutes / dayTime
+                : (currentTime.TotalMinutes - dayTime) / nightTime;
+
+            var r = isDay
+                ? dayColor.R + (nightColor.R - dayColor.R) * filledPercentage
+                : nightColor.R + (dayColor.R - nightColor.R) * filledPercentage;
+            var g = isDay
+                ? dayColor.G + (nightColor.G - dayColor.G) * filledPercentage
+                : nightColor.G + (dayColor.G - nightColor.G) * filledPercentage;
+            var b = isDay
+                ? dayColor.B + (nightColor.B - dayColor.B) * filledPercentage
+                : nightColor.B + (dayColor.B - nightColor.B) * filledPercentage;
+
+            var result = new Color((float) r, (float) g, (float) b);
+
+            return result;
+        }
+    }
+}
