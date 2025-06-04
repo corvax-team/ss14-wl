568e3bf8d2b8837229a6db6d7e50bc92c687f4fd
Pupchansky <139097101+Fanolli@users.noreply.github.com>
2025-06-04 16:15:06 +0500

    Minor locale fix

--- Content.Shared/_WL/CCVars/WLCCVars.cs
+++ Content.Shared/_WL/CCVars/WLCCVars.cs
@@ -134,8 +134,7 @@ public sealed class WLCVars
         CVarDef.Create("vote.evacuation_shuttle_vote_ratio", 0.6f, CVar.SERVERONLY);
 
     /// <summary>
-    /// Сколько требуется согласных игроков для вызова.
-    /// В процентах.
+    /// Время голосования.
     /// </summary>
     public static readonly CVarDef<int> VoteShuttleTimer =
         CVarDef.Create("vote.evacuation_shuttle_vote_time", 40, CVar.SERVERONLY);
