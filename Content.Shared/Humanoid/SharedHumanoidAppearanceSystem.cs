af20917855eb30798ba4bd8b95aa95090ea8b98f
ssdaniel24 <bo7oaonteg2m@mail.ru>
2025-09-15 11:15:42 +0300

    Fixed import

--- Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
+++ Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
@@ -23,6 +23,7 @@ using Robust.Shared.Serialization.Manager;
 using Robust.Shared.Serialization.Markdown;
 using Robust.Shared.Utility;
 using YamlDotNet.RepresentationModel;
+using Content.Shared.Sprite; // WL-Changes
 
 namespace Content.Shared.Humanoid;
 
