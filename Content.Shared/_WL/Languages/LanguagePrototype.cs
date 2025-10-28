9b0c4d0d804c50f11650898dad746016f26fabc8
Charlotte Tezuka <tau271@proton.me>
2025-10-29 00:14:27 +0200

    Android sounds

--- Content.Shared/_WL/Languages/LanguagePrototype.cs
+++ Content.Shared/_WL/Languages/LanguagePrototype.cs
@@ -1,3 +1,4 @@
+using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
@@ -24,6 +25,12 @@ public sealed partial class LanguagePrototype : IPrototype
     [DataField("color")]
     public Color Color = Color.White;
 
-    [DataField]
+    [DataField("needtts")]
     public bool NeedTTS = true;
+
+    [DataField("needsound")]
+    public bool NeedSound = false;
+
+    [DataField("sound")]
+    public SoundCollectionSpecifier Sound = new SoundCollectionSpecifier("TernarySounds");
 }
