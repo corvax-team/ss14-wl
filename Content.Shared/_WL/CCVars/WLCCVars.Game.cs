c9dbe0abbdff86830b61ccf11a87926a1a46912a
Litogin <litogin1@gmail.com>
2025-11-17 20:03:26 +0300

    splitting WLCCvar into separate logical group files

---
+++ Content.Shared/_WL/CCVars/WLCCVars.Game.cs
@@ -0,0 +1,21 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._WL.CCVars;
+
+/// <summary>
+///     WL modules console variables
+/// </summary>
+public sealed partial class WLCVars
+{
+    /// <summary>
+    /// Через сколько времени(в секундах) появится кнопка возвращения в лобби.
+    /// </summary>
+    public static readonly CVarDef<int> GhostReturnToLobbyButtonCooldown =
+        CVarDef.Create("ghost.return_to_lobby_button_cooldown", 600, CVar.SERVERONLY); //WL-changes  (10 minutes)
+
+    /// <summary>
+    /// Нужно ли проверять игрока на возраст при выборе роли.
+    /// </summary>
+    public static readonly CVarDef<bool> IsAgeCheckNeeded =
+        CVarDef.Create("game.is_age_check_needed", true, CVar.REPLICATED);
+}
