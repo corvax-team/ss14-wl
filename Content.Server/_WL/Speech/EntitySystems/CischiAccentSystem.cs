e119f9c08999cc5a0bbf47d07a106afd9323f581
Livsi <nkizilov275@gmail.com>
2026-06-09 15:52:53 +0500

    Revert "Фикс акцента"
    This reverts commit 08698df783661d2313bac87bd76622faddbe9950.

--- Content.Server/_WL/Speech/EntitySystems/CischiAccentSystem.cs
+++ Content.Server/_WL/Speech/EntitySystems/CischiAccentSystem.cs
@@ -6,18 +6,18 @@ namespace Content.Server._WL.Speech.EntitySystems
 {
     public sealed class CischiAccentSystem : EntitySystem
     {
-        private static readonly Regex ReplacementsYa = new Regex("я");
-        private static readonly Regex ReplacementsYaUpper = new Regex("Я");
-        private static readonly Regex ReplacementsYe = new Regex("е");
-        private static readonly Regex ReplacementsYeUpper = new Regex("Е");
-        private static readonly Regex ReplacementsYu = new Regex("ю");
-        private static readonly Regex ReplacementsYuUpper = new Regex("Ю");
-        private static readonly Regex ReplacementsC = new Regex("ц");
-        private static readonly Regex ReplacementsCUpper = new Regex("Ц");
-        private static readonly Regex ReplacementsSh = new Regex("щ");
-        private static readonly Regex ReplacementsShUpper = new Regex("Щ");
-        private static readonly Regex ReplacementsCh = new Regex("ч");
-        private static readonly Regex ReplacementsChUpper = new Regex("Ч");
+        private static readonly Regex ReplacementsYa = new Regex("йа");
+        private static readonly Regex ReplacementsYaUpper = new Regex("ЙА");
+        private static readonly Regex ReplacementsYe = new Regex("йэ");
+        private static readonly Regex ReplacementsYeUpper = new Regex("ЙЭ");
+        private static readonly Regex ReplacementsYu = new Regex("йу");
+        private static readonly Regex ReplacementsYuUpper = new Regex("ЙУ");
+        private static readonly Regex ReplacementsC = new Regex("тс");
+        private static readonly Regex ReplacementsCUpper = new Regex("ТС");
+        private static readonly Regex ReplacementsSh = new Regex("шь");
+        private static readonly Regex ReplacementsShUpper = new Regex("ШЬ");
+        private static readonly Regex ReplacementsCh = new Regex("дз");
+        private static readonly Regex ReplacementsChUpper = new Regex("ДЗ");
 
         public override void Initialize()
         {
@@ -28,18 +28,18 @@ namespace Content.Server._WL.Speech.EntitySystems
         {
             var message = args.Message;
 
-            message = ReplacementsYa.Replace(message, "йа");
-            message = ReplacementsYaUpper.Replace(message, "ЙА");
-            message = ReplacementsYe.Replace(message, "йэ");
-            message = ReplacementsYeUpper.Replace(message, "ЙЭ");
-            message = ReplacementsYu.Replace(message, "йу");
-            message = ReplacementsYuUpper.Replace(message, "ЙУ");
-            message = ReplacementsC.Replace(message, "тс");
-            message = ReplacementsCUpper.Replace(message, "ТС");
-            message = ReplacementsSh.Replace(message, "шь");
-            message = ReplacementsShUpper.Replace(message, "ШЬ");
-            message = ReplacementsCh.Replace(message, "дз");
-            message = ReplacementsChUpper.Replace(message, "ДЗ");
+            message = ReplacementsYa.Replace(message, "я");
+            message = ReplacementsYaUpper.Replace(message, "Я");
+            message = ReplacementsYe.Replace(message, "е");
+            message = ReplacementsYeUpper.Replace(message, "Е");
+            message = ReplacementsYu.Replace(message, "ю");
+            message = ReplacementsYuUpper.Replace(message, "Ю");
+            message = ReplacementsC.Replace(message, "ц");
+            message = ReplacementsCUpper.Replace(message, "Ц");
+            message = ReplacementsSh.Replace(message, "щ");
+            message = ReplacementsShUpper.Replace(message, "Щ");
+            message = ReplacementsCh.Replace(message, "ч");
+            message = ReplacementsChUpper.Replace(message, "Ч");
 
             args.Message = message;
         }
