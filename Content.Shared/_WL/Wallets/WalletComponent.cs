64f618eeb78844ed8bd64cb04a0e9d7cd5a44054
nukkuminen <90336027+oldschoolotaku@users.noreply.github.com>
2026-07-22 23:05:44 +0300

    retry

---
+++ Content.Shared/_WL/Wallets/WalletComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._WL.Wallet;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class WalletComponent : Component
+{
+    [DataField]
+    public string IdSlotId = "idSlot";
+
+    [ViewVariables, AutoNetworkedField]
+    public EntityUid? ContainedId;
+
+    [DataField]
+    public Vector2 CardOffset = new(-0.07f, -0.03f);
+
+    [DataField]
+    public Vector2 IdOffset = new(-0.17f, 0.02f);
+}
---
+++ Content.Shared/_WL/Wallets/WalletSystem.cs
@@ -0,0 +1,65 @@
+﻿using Content.Shared.Access.Components;
+using Content.Shared.Access.Systems;
+using Robust.Shared.Containers;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._WL.Wallet;
+
+public sealed partial class WalletSystem : EntitySystem
+{
+    [Dependency] private SharedAppearanceSystem _appearance = default!;
+    [Dependency] private SharedJobStatusSystem _jobStatus = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<WalletComponent, EntInsertedIntoContainerMessage>(OnIdInserted);
+        SubscribeLocalEvent<WalletComponent, EntRemovedFromContainerMessage>(OnIdRemoved);
+        SubscribeLocalEvent<WalletComponent, GetAdditionalAccessEvent>(OnGetAdditionalAccess);
+    }
+
+    private void OnIdInserted(Entity<WalletComponent> ent, ref EntInsertedIntoContainerMessage args)
+    {
+        UpdateIdSlot(ent, args.Container);
+    }
+
+    private void OnIdRemoved(Entity<WalletComponent> ent, ref EntRemovedFromContainerMessage args)
+    {
+        UpdateIdSlot(ent, args.Container);
+    }
+
+    private void OnGetAdditionalAccess(Entity<WalletComponent> ent, ref GetAdditionalAccessEvent args)
+    {
+        if (ent.Comp.ContainedId is { } id)
+            args.Entities.Add(id);
+    }
+
+    private void UpdateIdSlot(Entity<WalletComponent> ent, BaseContainer container)
+    {
+        if (container.ID != ent.Comp.IdSlotId)
+            return;
+
+        var hasId = container.ContainedEntities.Count > 0;
+        ent.Comp.ContainedId = hasId ? container.ContainedEntities[0] : null;
+
+        Dirty(ent);
+
+        _appearance.SetData(ent, WalletVisuals.HasId, hasId);
+        _jobStatus.UpdateStatus(Transform(ent).ParentUid);
+    }
+}
+
+[Serializable, NetSerializable]
+public enum WalletVisuals : byte
+{
+    HasId
+}
+
+[Serializable, NetSerializable]
+public enum WalletVisualLayers : byte
+{
+    IdBase,
+    IdIcon,
+    Frame
+}
