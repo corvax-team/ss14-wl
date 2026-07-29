1cebcfc3a50477fc722cc13ded49e61449e49d0a
modder123543 <gavniuk172010@gmail.com>
2026-07-29 05:35:21 +0300

    Emoteee

--- Content.Server/_WL/Silicons/StationAi/StationAiEmoteVisibilitySystem.cs
+++ Content.Server/_WL/Silicons/StationAi/StationAiEmoteVisibilitySystem.cs
@@ -27,12 +27,9 @@ public sealed partial class StationAiEmoteVisibilitySystem : EntitySystem
     {
 
         if (ev.ChatType != InGameICChatType.Emote)
-        {
             return;
-        }
 
         var coreQuery = EntityQueryEnumerator<StationAiCoreComponent>();
-        var range = 10;
 
         while (coreQuery.MoveNext(out var coreUid, out var core))
         {
@@ -52,17 +49,16 @@ public sealed partial class StationAiEmoteVisibilitySystem : EntitySystem
             if (core.RemoteEntity is not { } remoteEntity)
                 continue;
 
-            if (!_examine.InRangeUnOccluded(remoteEntity, ev.Source, range))
+            if (!_examine.InRangeUnOccluded(remoteEntity, ev.Source, ev.VoiceRange))
                 continue;
 
             if (!_examine.CanExamine(heldAi.Value, ev.Source))
                 continue;
 
             ev.Recipients[actor.PlayerSession] = new ChatSystem.ICChatRecipientData(
-                Range: 10f,
+                Range: ev.VoiceRange,
                 Observer: false
             );
-
         }
     }
 }
