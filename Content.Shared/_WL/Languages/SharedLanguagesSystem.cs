d846ba0481ccc72f5c0713789374ccd623ba2bd7
modder123543 <gavniuk172010@gmail.com>
2026-09-19 15:34:28 +0300

    hmhm///

--- Content.Shared/_WL/Languages/SharedLanguagesSystem.cs
+++ Content.Shared/_WL/Languages/SharedLanguagesSystem.cs
@@ -254,7 +254,10 @@ public abstract partial class SharedLanguagesSystem : EntitySystem
             return false;
 
         if (!TryComp<LanguagesComponent>(source, out var comp))
-            return false;
+        {
+            newMessage = _chat.SanitizeMessageCapital(message.Trim()) ?? string.Empty;
+            return !string.IsNullOrWhiteSpace(newMessage);
+        }
 
         ProtoId<LanguagePrototype>? languageId;
 
