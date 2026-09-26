4f757d95f8b0a214d2c000063bf9d585b654b65a
teishipng <teishipng@proton.me>
2026-09-26 18:35:38 +0300

    THE GREAT SOCKS COMEBACK

---
+++ Content.Server.Database/Migrations/Postgres/20260926132951_Socks.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class Socks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+             migrationBuilder.Sql(
+                "UPDATE profile SET organ_markings = organ_markings #- '{Torso,Socks}' WHERE organ_markings IS NOT NULL");
+        }
+    }
+}
